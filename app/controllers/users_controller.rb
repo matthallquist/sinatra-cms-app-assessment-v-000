1ad96c928af5ed053b23acf06ff0814a8fb14bfb
Matthew Hallquist <matthallquist@yahoo.com>
2017-03-10 23:43:13 +0000

    signup and logout work. cannot get to signup while logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,19 +9,46 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-
+    if params[:password] == "" || params[:email] == "" || params[:username] == ""
+      redirect to '/signup'
+    else
+      if User.find_by(:username => params[:username])
+        #need error here 'username exists'
+        redirect to '/signup'
+      else
+        @user = User.create(:username => params[:username], :password => params[:password])
+        @user.save
+        session[:user_id] = @user.id
+        redirect to '/recipes'
+      end
+    end
   end
 
   get '/login' do
-
+    if logged_in
+      redirect to '/recipes'
+    else
+      erb :'/users/login'
+    end
   end
 
   post '/login' do
-
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/recipes'
+    else
+      redirect to '/signup'
+    end
   end
 
   get '/logout' do
-
+    if logged_in
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
   end
 
   get '/users/:id' do
