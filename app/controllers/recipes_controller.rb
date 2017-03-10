6ffe8a29100e2d506fbf403e8221ef8f2f5217d7
Matthew Hallquist <matthallquist@yahoo.com>
2017-03-10 22:48:32 +0000

    created form for recipes, stops invalid params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,7 +10,13 @@ class RecipesController < ApplicationController
   end
 
   post '/recipes' do
-
+    if params[:name] == "" || params[:category] == "" || params[:prep_time] == "" || params[:cook_time] == "" || params[:ingredients] == "" || params[:instructions] == ""
+      redirect to '/recipes/new'
+    else
+      @recipe = Recipe.create(name: params[:name], category: params[:category], prep_time: params[:prep_time], cook_time: params[:cook_time], ingredients: params[:ingredients], instructions: params[:instructions])
+      @recipe.save
+      redirect to '/recipes'
+    end
   end
 
   get '/recipes/:id' do
