a3c361a2acd85e681f22f25af5d98f2733dfbc05
Matthew Hallquist <matthallquist@yahoo.com>
2017-03-10 22:15:57 +0000

    built the skeleton for RecipesController

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,35 @@
 class RecipesController < ApplicationController
 
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :'/recipes/recipes'
+  end
+
+  get '/recipes/new' do
+    erb :'/recipes/new'
+  end
+
+  post '/recipes' do
+
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by(:id => params[:id])
+    erb :'/recipes/show'
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by(:id => params[:id])
+    erb :'/recipes/edit'
+  end
+
+  patch '/recipes/:id' do
+
+  end
+
+  delete '/recipes/:id/delete' do
+
+  end
+
+
 end
