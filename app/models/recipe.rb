0f4f1c6f5a66214f894ad20a4a9351ecfe7f7c8d
Matthew Hallquist <matthallquist@yahoo.com>
2017-03-14 19:23:25 +0000

    added avg rating score to Recipe class, and added the score to the recipes index page along with amount of reviews

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,17 @@ class Recipe < ActiveRecord::Base
   has_many :reviews
   belongs_to :user
   validates :name, :category, :prep_time, :cook_time, :ingredients, :instructions, presence: true
+
+  def rating_avg
+    if self.reviews.any?
+      score = 0
+      self.reviews.each do |review|
+        score += review.rating
+      end
+      score = score / self.reviews.count.to_f
+    else
+      score = 0
+    end
+    score
+  end
 end
