6053e99b5052a74220657f24102e46cab36d2569
Matthew Hallquist <matthallquist@yahoo.com>
2017-03-23 16:00:02 +0000

    no special characters for recipe form except ingredients and instructions

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,8 @@ class Recipe < ActiveRecord::Base
   has_many :reviews
   belongs_to :user
   validates :name, :category, :prep_time, :cook_time, :ingredients, :instructions, presence: true
+  validates :name, :category, :prep_time, :cook_time, format: { with: /\A[a-zA-Z\d]+\z/,
+    message: "only allows letters and numbers" }
 
   def rating_avg
     if self.reviews.any?
